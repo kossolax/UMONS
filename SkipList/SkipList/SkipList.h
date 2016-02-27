7fdd27a2bc4b76601525129b69bf6b7cc70afa45
kossolax <kossolax@gmail.com>
2016-02-27 10:51:07 +0100

    màj signature func

--- SkipList/SkipList/SkipList.h
+++ SkipList/SkipList/SkipList.h
@@ -7,7 +7,7 @@
 #include <time.h>
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
-#define DEBUG
+//#define DEBUG
 
 typedef struct SkipList {
 	int levelMAX;
@@ -22,13 +22,14 @@ typedef struct node {
 
 } node;
 
-SkipList SK_init(int maxElem, float p);
-void SK_free(SkipList list);
-node* SK_Search(SkipList list, int key);
-int SK_Insert(SkipList list, int key, int value);
-int SK_Delete(SkipList list, int key);
+SkipList* SK_init(int maxElem, float p);
+void SK_free(SkipList* list);
+node* SK_Search(SkipList* list, int key);
+int SK_Insert(SkipList* list, int key, int value);
+int SK_Delete(SkipList* list, int key);
 int SK_GetValueFromNode(node* noeud);
 
 // Private: eyes only.
-node* createNode(SkipList, int key, int value);
-int getRandomLevel(SkipList list);
+node* createNode(SkipList*, int key, int value);
+void SK_Print(SkipList* list);
+int getRandomLevel(SkipList* list);
