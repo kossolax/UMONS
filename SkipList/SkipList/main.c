86d621d1822707b910e86723282c91933d58d786
kossolax <kossolax@gmail.com>
2016-02-27 09:31:51 +0100

    nouveau scénario
    La suppression d'un prédécesseur pose soucis

--- SkipList/SkipList/main.c
+++ SkipList/SkipList/main.c
@@ -5,19 +5,32 @@
 int main(int argc, char** argv) {
 	SkipList list = SK_init(32, 0.25);
 
+	SK_Insert(list, 1, 1);
 	for (int i = 4; i <= 8; i++)
 		SK_Insert(list, i, i);
 
-	SK_Delete(list, 6);
+	SK_Delete(list, 4);
 	node* found = SK_Search(list, 5);
 	node* notfound = SK_Search(list, 6);
 
 	SK_free(list);
 
-	if (!found)
+	if (!found) {
+		printf("ERR: 5 was not found but exist\n");
+#ifdef WIN32
+		system("pause");
+#endif
 		return 1;
-	if (notfound)
+	}
+	if (notfound) {
+		printf("ERR: 6 was found but doesn't exist\n");
+#ifdef WIN32
+		system("pause");
+#endif
 		return 1;
+	}
+
+	printf("OK\n");
 
 #ifdef WIN32
 	system("pause");
