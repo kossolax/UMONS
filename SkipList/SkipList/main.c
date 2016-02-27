1d9bd76cce103e7167d0adfd2ab66c1c6df15334
kossolax <kossolax@gmail.com>
2016-02-27 10:50:51 +0100

    nouveau scénario

--- SkipList/SkipList/main.c
+++ SkipList/SkipList/main.c
@@ -3,33 +3,45 @@
 #include "SkipList.h"
 
 int main(int argc, char** argv) {
-	SkipList* list = SK_init(32, 0.25);
 
-	for (int i = 1; i <= 16; i++)
-		SK_Insert(list, i, i);
+	for (int j = 0; j <= 1000; j++) {
+		if (j % 100 == 0)
+			printf(".");
+		
+		SkipList* list = SK_init(32, 0.25);
 
-	SK_Print(list);
-	SK_Delete(list, 4);
-	SK_Print(list);
+		for (int i = 1; i <= 32; i++)
+			SK_Insert(list, i, i);
 
-	node* found = SK_Search(list, 5);
-	node* notfound = SK_Search(list, 6);
+		SK_Delete(list, 4);
+		SK_Delete(list, 6);
+		SK_Delete(list, 9);
+		for (int i = 11; i <= 24; i++)
+			SK_Delete(list, i);
+		
+		node* found = SK_Search(list, 5);
+		node* notfound = SK_Search(list, 6);
 
-	SK_free(list);
-
-	if (!found) {
-		printf("ERR: 5 was not found but exist\n");
+		if (!found) {
+			SK_Print(list);
+			SK_free(list);
+			printf("ERR: 5 was not found but exist\n");
 #ifdef WIN32
-		system("pause");
+			system("pause");
 #endif
-		return 1;
-	}
-	if (notfound) {
-		printf("ERR: 6 was found but doesn't exist\n");
+			return 1;
+		}
+		if (notfound) {
+			SK_Print(list);
+			SK_free(list);
+			printf("ERR: 6 was found but doesn't exist\n");
 #ifdef WIN32
-		system("pause");
+			system("pause");
 #endif
-		return 1;
+			return 1;
+		}
+
+		SK_free(list);
 	}
 
 	printf("OK\n");
