4d8daa49b89788a460118771ca2d7f93b1648b57
kossolax <kossolax@gmail.com>
2016-03-11 11:39:22 +0100

    junit

---
+++ UML/src/tests/Articles.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Articles {
+
+	@Test
+	public void testIsAvalaible() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDelivery() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ UML/src/tests/Machine.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Machine {
+
+	@Test
+	public void testBuy() {
+		fail("Not yet implemented");
+	}
+
+}
