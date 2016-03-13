e50f72a224d6ff9775e283fac5842646736e15aa
kossolax <kossolax@gmail.com>
2016-03-13 11:02:52 +0100

    correction testBuy

--- UML/src/tests/Machine.java
+++ UML/src/tests/Machine.java
@@ -19,14 +19,20 @@ public class Machine {
 
 	@Test
 	public void testBuy() {
-		Stockage stock = new Classic(8, 25);
-		Collection<RawMaterial> recette = new ArrayList<RawMaterial>();
-		recette.add(new RawMaterial(5, TypeOfRawMaterial.gram, stock));
-		Article cafe = new Article("Café", 1.0, new Recipe(recette));
-		
-		framework.Machine machine = new framework.Machine();
-		assertTrue(machine.Buy(new Token(), cafe));
-		assertFalse(machine.Buy(new Token(), cafe));
+		try {
+			Stockage stock = new Classic(100);
+			RawMaterial cofeee = new RawMaterial(5, TypeOfRawMaterial.gram, stock);
+			stock.Add(cofeee);
+			Collection<RawMaterial> recette = new ArrayList<RawMaterial>();
+			recette.add(cofeee);
+			Article cafe = new Article("Café", 1.0, new Recipe(recette));
+			
+			framework.Machine machine = new framework.Machine();
+			assertTrue(machine.Buy(new Token(), cafe));
+			assertFalse(machine.Buy(new Token(), cafe));
+		} catch (Exception e ) {
+			fail(e.getMessage());
+		}
 	}
 
 }
