27ab1284784f9738bb7c35b57e11e28f8176303d
kossolax <kossolax@gmail.com>
2016-05-06 18:07:22 +0200

    Correction compil unix

--- SkipList/SkipList/main.c
+++ SkipList/SkipList/main.c
@@ -4,34 +4,45 @@
 #include "SkipList.h"
 #include "LinkList.h"
 
+int int_pow(int base, int exp);
+
 int main(int argc, char** argv) {
 	srand((int)time(NULL));
 	clock_t begin, end;
-	const int maxTest = 1;
-	const int MaxSize = int_pow(2, 16);
+	int maxTest, maxSize;
+	if( argc != 3 ) {
+	 	maxTest	= 10;
+		maxSize = int_pow(2, 8);
+	}
+	else {
+		maxTest = atoi(argv[1]);
+		maxSize = atoi(argv[2]);
+	}
+
+
 
 	begin = clock();
 	for (int i = 1; i <= maxTest; i++) {
-		SkipList* list = SK_init(MaxSize, 0.5f);
-		for (int j = 1; j <= MaxSize; j++)
+		SkipList* list = SK_init(maxSize, 0.5f);
+		for (int j = 1; j <= maxSize; j++)
 			SK_Insert(list, j, j);
 		SK_free(list);
 	}
 	end = clock();
 	printf("%lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
-	
+
 	begin = clock();
 	for (int i = 1; i <= maxTest; i++) {
 		LinkList* list = LL_init();
-		for (int j = 1; j <= MaxSize; j++)
+		for (int j = 1; j <= maxSize; j++)
 			list = LL_Insert(list, j, j);
-		//LL_Print(list);
 		list = LL_free(list);
 	}
 	end = clock();
 	printf("%lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
-
+#if _WIN32
 	system("pause");
+#endif
 	return 0;
 }
 int int_pow(int base, int exp) {
