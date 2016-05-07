aa0a5d2e5703e4d24d241f9d27726194feb31499
kossolax <kossolax@gmail.com>
2016-05-07 19:17:57 +0200

    scénario test

--- SkipList/SkipList/main.c
+++ SkipList/SkipList/main.c
@@ -8,55 +8,97 @@
 
 int main(int argc, char** argv) {
 	srand((int)time(NULL));
-	clock_t begin, end;
-	int maxSize = 1, i, j;
+	clock_t begin;
+	int i, j, k, timeout[5] = { 0, 0, 0, 0 }, maxSize = 1;
+	double timer;
 	int maxTest = ( argc >= 2 ? atoi(argv[1]) : 1 );
-	int maxIteration = ( argc >= 3 ? atoi(argv[2]) : 8 );
+	int maxIteration = ( argc >= 3 ? atoi(argv[2]) : 24 );
 	float p = ( argc >= 4 ? atof(argv[3]) : 0.5f );
+	double maxTime = (argc >= 5 ? atof(argv[4]) : 60.0);
+	setbuf(stdout, NULL);
+
 	printf("iter.\telems\tSkipList\tHashTable\tBinaryTree\tLinkList\n");
 
-	for (int a = 1; a <= maxIteration; a++) {
+	for (k = 1; k <= maxIteration; k++) {
 		maxSize *= 2;
 		printf("%d\t%d\t", maxTest, maxSize);
 
-		begin = clock();
-		for (i = 1; i <= maxTest; i++) {
-			SkipList* list = SK_init(maxSize, p);
-			for (j = 1; j <= maxSize; j++)
-				SK_Insert(list, rand() % j, j);
-			SK_free(&list);
-		}
-		end = clock();
-		printf("%lf\t", (double)(end - begin) / CLOCKS_PER_SEC);
 
-		begin = clock();
-		for (i = 1; i <= maxTest; i++) {
-			HashTable* list = HT_init(maxSize, 0.8);
-			for (j = 1; j <= maxSize; j++)
-				HT_Insert(list, rand() % j, j);
-			HT_free(&list);
-		}
-		end = clock();
-		printf("%lf\t", (double)(end - begin) / CLOCKS_PER_SEC);
-
-		begin = clock();
-		for (i = 1; i <= maxTest; i++) {
-			Tree* list = TR_init();
-			for (j = 1; j <= maxSize; j++)
-				TR_Insert(&list, rand() % j, j);
-			TR_free(&list);
-		}
-		end = clock();
-		printf("%lf\t", (double)(end - begin) / CLOCKS_PER_SEC);
+		if (timeout[0] == 0) {
 			begin = clock();
-		for (i = 1; i <= maxTest; i++) {
-			LinkList* list = LL_init();
-			for (j = 1; j <= maxSize; j++)
-				LL_Insert(&list, rand() % j, j);
-			LL_free(&list);
+			for (i = 1; i <= maxTest; i++) {
+				SkipList* list = SK_init(maxSize, p);
+				for (j = 1; j <= maxSize; j++)
+					SK_Insert(list, rand() % j, j);
+				SK_free(&list);
+			}
+			timer = (double)(clock() - begin) / CLOCKS_PER_SEC;
+			printf("%lf", timer);
+			if (timer > maxTime)
+				timeout[0] = 1;
 		}
-		end = clock();
-		printf("%lf", (double)(end - begin) / CLOCKS_PER_SEC);
+		else {
+			printf("-TIMEOUT-");
+		}
+
+
+
+		printf("\t");
+		if (timeout[1] == 0) {
+			begin = clock();
+			for (i = 1; i <= maxTest; i++) {
+				HashTable* list = HT_init(maxSize, 0.8);
+				for (j = 1; j <= maxSize; j++)
+					HT_Insert(list, rand() % j, j);
+				HT_free(&list);
+			}
+			timer = (double)(clock() - begin) / CLOCKS_PER_SEC;
+			printf("%lf", timer);
+			if (timer > maxTime)
+				timeout[1] = 1;
+		}
+		else {
+			printf("-TIMEOUT-");
+		}
+
+
+
+		printf("\t");
+		if (timeout[2] == 0) {
+			begin = clock();
+			for (i = 1; i <= maxTest; i++) {
+				Tree* list = TR_init();
+				for (j = 1; j <= maxSize; j++)
+					TR_Insert(&list, rand() % j, j);
+				TR_free(&list);
+			}
+			timer = (double)(clock() - begin) / CLOCKS_PER_SEC;
+			printf("%lf", timer);
+			if (timer > maxTime)
+				timeout[2] = 1;
+		}
+		else {
+			printf("-TIMEOUT-");
+		}
+
+
+		printf("\t");
+		if (timeout[3] == 0) {
+			begin = clock();
+			for (i = 1; i <= maxTest; i++) {
+				LinkList* list = LL_init();
+				for (j = 1; j <= maxSize; j++)
+					LL_Insert(&list, rand() % j, j);
+				LL_free(&list);
+			}
+			timer = (double)(clock() - begin) / CLOCKS_PER_SEC;
+			printf("%lf", timer);
+			if (timer > maxTime)
+				timeout[3] = 1;
+		}
+		else {
+			printf("-TIMEOUT-");
+		}
 
 		printf("\n");
 	}
