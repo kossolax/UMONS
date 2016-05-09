06fe2d13b741c8930db247d2a6c3c31cb2898c5a
kossolax <kossolax@gmail.com>
2016-05-09 10:27:14 +0200

    mêmes clés entre les tests

--- SkipList/SkipList/main.c
+++ SkipList/SkipList/main.c
@@ -15,28 +15,36 @@ int main(int argc, char** argv) {
 	int i, j, k, timeout[5] = { 0, 0, 0, 0 }, maxSize = 1;
 	double timer;
 	size_t maxMemoryUsage = 0, memory = 0;
-	int maxTest = ( argc >= 2 ? atoi(argv[1]) : 1 );
-	int maxIteration = ( argc >= 3 ? atoi(argv[2]) : 24 );
-	float p = ( argc >= 4 ? atof(argv[3]) : 0.5f );
-	double maxTime = (argc >= 5 ? atof(argv[4]) : 60.0);
+	int maxTest = (argc >= 2 ? atoi(argv[1]) : 1);
+	int maxIteration = (argc >= 3 ? atoi(argv[2]) : 24);
+	float p = (argc >= 4 ? atof(argv[3]) : 0.5f);
+	double maxTime = (argc >= 5 ? atof(argv[4]) : 1.0);
+	int** keys;
 	setbuf(stdout, NULL);
 
 	printf("%13s | %25s | %25s | %25s | %25s |\n", "operations", "SkipList         ", "HashTable        ", "BinaryTree         ", "LinkList         ");
-	printf("%13s | %25s | %25s | %25s | %25s |\n", "it   elems", "TIME   |   MEMORY  ", "TIME   |   MEMORY  ", "TIME   |   MEMORY  ", "TIME   |   MEMORY  ");
+	printf("%13s | %25s | %25s | %25s | %25s |\n", "iter.   elems", "TIME   |   MEMORY  ", "TIME   |   MEMORY  ", "TIME   |   MEMORY  ", "TIME   |   MEMORY  ");
 
 	for (k = 1; k <= maxIteration; k++) {
 		maxSize *= 2;
+		keys = (int**)malloc(sizeof(int*) * maxTest);
+		for (i = 0; i < maxTest; i++) {
+			keys[i] = (int*)malloc(sizeof(int) * maxSize);
+			for (j = 0; j < maxSize; j++)
+				keys[i][j] = rand() % (j + 1);
+		}
+
 		printf("%3d %9d | ", maxTest, maxSize);
 
 		if (timeout[0] == 0) {
 			begin = clock();
-			for (i = 1; i <= maxTest; i++) {
+			for (i = 0; i < maxTest; i++) {
 				SkipList* list = SK_init(maxSize, p);
-				for (j = 1; j <= maxSize; j++)
-					SK_Insert(list, rand() % j, j);
-				
+				for (j = 0; j < maxSize; j++)
+					SK_Insert(list, keys[i][j], j);
+
 				memory = getCurrentRSS();
-				if (memory > maxMemoryUsage ) maxMemoryUsage = memory;
+				if (memory > maxMemoryUsage) maxMemoryUsage = memory;
 				SK_free(&list);
 			}
 			timer = (double)(clock() - begin) / CLOCKS_PER_SEC;
@@ -44,17 +52,17 @@ int main(int argc, char** argv) {
 			if (timer > maxTime) timeout[0] = 1;
 		}
 		else {
-			printf("  -TIMEOUT-  | -TIMEOUT - | ");
+			printf("  -TIMEOUT-  | -TIMEOUT-  | ");
 		}
 
 
 		maxMemoryUsage = 0;
 		if (timeout[1] == 0) {
 			begin = clock();
-			for (i = 1; i <= maxTest; i++) {
+			for (i = 0; i < maxTest; i++) {
 				HashTable* list = HT_init(128, 4);
-				for (j = 1; j <= maxSize; j++)
-					HT_Insert(&list, rand() % j, j);
+				for (j = 0; j < maxSize; j++)
+					HT_Insert(&list, keys[i][j], j);
 				memory = getCurrentRSS();
 				if (memory > maxMemoryUsage) maxMemoryUsage = memory;
 				HT_free(&list);
@@ -64,7 +72,7 @@ int main(int argc, char** argv) {
 			if (timer > maxTime) timeout[1] = 1;
 		}
 		else {
-			printf("  -TIMEOUT-  | -TIMEOUT - | ");
+			printf("  -TIMEOUT-  | -TIMEOUT-  | ");
 		}
 
 
@@ -72,10 +80,10 @@ int main(int argc, char** argv) {
 		maxMemoryUsage = 0;
 		if (timeout[2] == 0) {
 			begin = clock();
-			for (i = 1; i <= maxTest; i++) {
+			for (i = 0; i < maxTest; i++) {
 				Tree* list = TR_init();
-				for (j = 1; j <= maxSize; j++)
-					TR_Insert(&list, rand() % j, j);
+				for (j = 0; j < maxSize; j++)
+					TR_Insert(&list, keys[i][j], j);
 				memory = getCurrentRSS();
 				if (memory > maxMemoryUsage) maxMemoryUsage = memory;
 				TR_free(&list);
@@ -85,17 +93,17 @@ int main(int argc, char** argv) {
 			if (timer > maxTime) timeout[2] = 1;
 		}
 		else {
-			printf("  -TIMEOUT-  | -TIMEOUT - | ");
+			printf("  -TIMEOUT-  | -TIMEOUT-  | ");
 		}
 
 
 		maxMemoryUsage = 0;
 		if (timeout[3] == 0) {
 			begin = clock();
-			for (i = 1; i <= maxTest; i++) {
+			for (i = 0; i < maxTest; i++) {
 				LinkList* list = LL_init();
-				for (j = 1; j <= maxSize; j++)
-					LL_Insert(&list, rand() % j, j);
+				for (j = 0; j < maxSize; j++)
+					LL_Insert(&list, keys[i][j], j);
 				if (memory > maxMemoryUsage) maxMemoryUsage = memory;
 				LL_free(&list);
 			}
@@ -104,10 +112,14 @@ int main(int argc, char** argv) {
 			if (timer > maxTime) timeout[3] = 1;
 		}
 		else {
-			printf("  -TIMEOUT-  | -TIMEOUT - | ");
+			printf("  -TIMEOUT-  | -TIMEOUT-  | ");
 		}
 
 		printf("\n");
+
+		for (i = 0; i < maxTest; i++)
+			free(keys[i]);
+		free(keys);
 	}
 
 #if _WIN32
